2068a55fc7fc5a39f9d26472b50d9f388c2092b5
gandharv <gandharv.suri@iiitb.org>
2020-07-01 16:07:38 +0530

    Bike Sharig Project

--- Project: Predicting bike sharing patterns/my_answers.py
+++ Project: Predicting bike sharing patterns/my_answers.py
@@ -144,7 +144,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 3000
-learning_rate = 0.05
-hidden_nodes = 28
+iterations = 5000
+learning_rate = 0.5
+hidden_nodes = 20
 output_nodes = 1
